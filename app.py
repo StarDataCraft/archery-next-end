5f617cf575f6b953f348ba7841b7aa391e749e42
LingJi <guiyingjournal@gmail.com>
2026-03-03 20:17:48 +0900

    Revert "Update app.py"
    This reverts commit 28c8ed5c1bb4bf8bacc29fc477f87286124f36e0.

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 import streamlit as st
 
-from src.state import init_state
+from src.state import init_state, goto_step
 from src.ui_language import render_language_step
 from src.ui_handedness import render_handedness_step
 from src.ui_analyze import render_analyze_step
 
+st.set_page_config(page_title="Archery Next End Coach", layout="wide")
+
 
 def main():
-    st.set_page_config(page_title="Archery Next End", layout="wide")
     init_state()
 
     step = st.session_state.step
+
     if step == "language":
         render_language_step()
     elif step == "handedness":
         render_handedness_step()
-    else:
+    elif step == "analyze":
         render_analyze_step()
+    else:
+        goto_step("language")
+        st.rerun()
 
 
 if __name__ == "__main__":
